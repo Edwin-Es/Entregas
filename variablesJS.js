11b14169f9e13b3d8ad9d115031608b9f9e92d3e
Edwin ED <edwin.estradafreelance@gmail.com>
2023-03-08 09:55:43 -0600

    Entrega variable

---
+++ variablesJS.js
@@ -0,0 +1,12 @@
+let tunombre="Edwin Estrada";
+let edad=23;
+let eresdesarrollador=true;
+let fechanacimiento= new Date('April 12,1999');
+const librofavorito={
+    titulo:"RELATOS BREVES 2015",
+    autor:"EDICIONES TAGUS ",
+    fecha:"2",
+    url:"https://www.casadellibro.com/ebook-relatos-breves-2015-ebook/cdlet00000002/2571021",
+}
+
+console.log(tunombre);
