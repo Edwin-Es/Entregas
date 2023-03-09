f808ab446abb6d099aaced0fe20626d130fb73fd
Edwin ED <edwin.estradafreelance@gmail.com>
2023-03-09 10:04:05 -0600

    Entrega bucles

---
+++ buclesjs/factorial-break.js
@@ -0,0 +1,10 @@
+let result=1;
+let i=1;
+while(i<=15){
+    result=result*i;
+    console.log(result);
+    i++;
+    if(i>10){
+        break;
+    }
+}
---
+++ buclesjs/factorial-while.js
@@ -0,0 +1,7 @@
+let result=1;
+let i=1;
+while(i<=10){
+    result=result*i;
+    console.log(result);
+    i++;
+}
