a9a36b566ace7fee7b76065ef93ecb6b814e5aab
Edwin ED <edwin.estradafreelance@gmail.com>
2023-03-10 10:50:45 -0600

    String entrega

---
+++ string.js
@@ -0,0 +1,14 @@
+let nombre="Edwin";
+let apellido="Estrada";
+let estudiante=`${nombre} ${apellido}`;
+let estudianteMayus= estudiante.toUpperCase();
+let estudianteMinus= estudiante.toLowerCase();
+let estudiantecount=estudiante.length;
+let primerletra=nombre.charAt([0]);
+let ultimaletra=apellido.charAt(apellido.length -1);
+let estudiantesinespacio=estudiante.trim();
+let contiene=estudiante.includes("Edwin");
+
+
+
+
