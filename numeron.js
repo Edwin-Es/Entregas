148dbc138222c0ffbcf37d07c3ae1dcbfe57b8b5
Edwin ED <edwin.estradafreelance@gmail.com>
2023-03-10 14:04:36 -0600

    entrega numeros

---
+++ numeron.js
@@ -0,0 +1,13 @@
+let altura=170;
+let alturametros=1.70;
+let peso=85.6;
+let alturaredondeada= alturametros.toFixed(1);
+let pesoredondeado=peso.toPrecision(5);
+let maximo=Number.MAX_VALUE;
+let maximo1=Number.MAX_VALUE + 1;
+
+if(maximo===maximo1){
+    console.log(true)
+}
+else{console.log(false)}
+
